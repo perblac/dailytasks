c1e56fff90b42e692a683f31036e9cbc5a0bc451
perblac <yhana.kromx@gmail.com>
2024-04-02 11:14:29 +0200

    install @angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +16,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"diariopracticasgrado","appId":"1:334552631074:web:16273521f063a04a0f477b","storageBucket":"diariopracticasgrado.appspot.com","apiKey":"AIzaSyD3om1fbCRNFikH4r5ObGA3v6hEZgHZWyY","authDomain":"diariopracticasgrado.firebaseapp.com","messagingSenderId":"334552631074"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
