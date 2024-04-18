d8ecd6480bd209142b43b1d51fad76bde8793050
perblac <yhana.kromx@gmail.com>
2024-04-18 12:13:13 +0200

    adjust login header colors and dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, Optional} from '@angular/core';
 import {IonRouterOutlet, Platform} from "@ionic/angular"
+import {Subscription} from "rxjs";
+import {TranslocoService} from "@jsverse/transloco";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,11 +10,20 @@ import {IonRouterOutlet, Platform} from "@ionic/angular"
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  private translationSubscription: Subscription;
+
   constructor(
     private platform: Platform,
+    private translocoService: TranslocoService,
+    private title: Title,
     @Optional() private routerOutlet?: IonRouterOutlet
   ) {
     // Initialize dark mode according to system preferences
     document.body.classList.toggle('dark', window.matchMedia('(prefers-color-scheme: dark)').matches);
+    // Subscribe to translation changes to change page tittle according
+    this.translationSubscription = this.translocoService.selectTranslation().subscribe(() => {
+      this.title.setTitle(this.translocoService.translate('headTitle'));
+    })
   }
 }
